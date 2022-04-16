ffa4d7c9329451c1112ffbf2f2182abdad3636e0
Julio Truzzi <julio.truzzi@gmail.com>
2022-04-16 01:26:25 -0300

    Start technology section

--- pages/Home/Technology/Technology.tsx
+++ pages/Home/Technology/Technology.tsx
@@ -1,17 +1,38 @@
 import type { NextPage } from "next";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
 
 const Technology: NextPage = () => (
-  <Flex
-    w="100%"
-    p="17px"
-    fontWeight="semibold"
-    fontSize="md"
-    position="relative"
-    justifyContent="center"
-  >
-    <Box>Technology (TBD)</Box>
-  </Flex>
+  <Box bgColor="#1E293B" textColor={"white"}>
+    <Container maxW="container.2xl" pb="10rem" px={["3rem", "10rem"]}>
+      <Flex
+        pt={["50px"]}
+        fontSize="md"
+        position="relative"
+        justifyContent="center"
+      >
+        <Box textAlign="left" w="full">
+          <Text
+            color="secondary"
+            fontFamily={"Inter"}
+            letterSpacing="wider"
+            fontWeight="semibold"
+          >
+            TECHNOLOGY
+          </Text>
+          <Box
+            textStyle="h1"
+            pt={["15px", "15px"]}
+            pb={["43px", "43px"]}
+            lineHeight={"110%"}
+          >
+            Meta Pool,
+            <br />
+            superior staking
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
+  </Box>
 );
 
 export default Technology;
