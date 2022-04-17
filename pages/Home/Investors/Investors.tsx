25ddc42ec15aba60253d81fa01285a923497233b
Julio Truzzi <julio.truzzi@gmail.com>
2022-04-17 15:11:00 -0300

    make investors responsive

--- pages/Home/Investors/Investors.tsx
+++ pages/Home/Investors/Investors.tsx
@@ -11,45 +11,39 @@ import {
 } from "@chakra-ui/react";
 
 const Investors: NextPage = () => (
-  <Flex
-    fontSize="md"
-    position="relative"
-    justifyContent="center"
-    fontFamily={"Inter"}
+  <Container
+    maxW="container.xl"
+    px={["3rem", "10rem"]}
+    py="100px"
+    textAlign="center"
   >
-    <Container maxW="container.2xl" px={["3rem", "10rem"]} py="100px">
-      <Box textAlign="center">
-        <Text
-          color="secondary"
-          fontFamily={"Inter"}
-          letterSpacing="wider"
-          fontWeight="semibold"
-        >
-          INVESTORS
-        </Text>
-        <Box
-          textStyle="h2"
-          pt={["15px", "15px"]}
-          pb={["43px", "43px"]}
-          noOfLines={[2, 2]}
-          fontFamily="Space Grotesk"
-          lineHeight="56px"
-        >
-          Backed by great people
-        </Box>
-        <Center my="122px">
-          <SimpleGrid columns={[1, 6]} spacing={10} mx={10}>
-            <Image alt="ATT" src="/icons/investors-dragonfly.svg" />
-            <Image alt="Farming" src="/icons/investors-att.svg" />
-            <Image alt="Farming" src="/icons/investors-move.svg" />
-            <Image alt="Farming" src="/icons/investors-svc.svg" />
-            <Image alt="Farming" src="/icons/investors-di.svg" />
-            <Image alt="Farming" src="/icons/investors-blockwall.svg" />
-          </SimpleGrid>
-        </Center>
-      </Box>
-    </Container>
-  </Flex>
+    <Text
+      color="secondary"
+      fontFamily={"Inter"}
+      letterSpacing="wider"
+      fontWeight="semibold"
+    >
+      INVESTORS
+    </Text>
+    <Box
+      textStyle="h2"
+      pt={["15px", "15px"]}
+      pb={["43px", "43px"]}
+      noOfLines={[2, 2]}
+      fontFamily="Space Grotesk"
+      lineHeight="56px"
+    >
+      Backed by great people
+    </Box>
+    <SimpleGrid columns={[3, 6]} spacing={10} my="100px" alignItems={"center"}>
+      <Image alt="ATT" src="/icons/investors-dragonfly.svg" />
+      <Image alt="Farming" src="/icons/investors-att.svg" />
+      <Image alt="Farming" src="/icons/investors-move.svg" />
+      <Image alt="Farming" src="/icons/investors-svc.svg" />
+      <Image alt="Farming" src="/icons/investors-di.svg" />
+      <Image alt="Farming" src="/icons/investors-blockwall.svg" />
+    </SimpleGrid>
+  </Container>
 );
 
 export default Investors;
