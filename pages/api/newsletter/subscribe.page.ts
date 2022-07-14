5f9a080fdfd4dd6b15bf74894711fc5d8a540235
Julio Truzzi <julio.truzzi@gmail.com>
2022-07-13 23:07:12 -0300

    Create vercel config

--- pages/api/newsletter/subscribe.page.ts
+++ pages/api/newsletter/subscribe.page.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["POST", "HEAD"],
+});
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 mailchimp.setConfig({
@@ -7,14 +12,27 @@ mailchimp.setConfig({
   server: process.env.MAILCHIMP_SERVER_PREFIX,
 });
 
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  fn: (arg0: any, arg1: any, arg2: (result: unknown) => void) => void
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    res.status(405).json({ ok: false, message: "Only POST requests allowed" });
-    return;
-  }
+  await runMiddleware(req, res, cors);
 
   try {
     const { email } = req.body;
